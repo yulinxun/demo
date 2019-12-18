5bb43fd9dec2a493958275bcf4ff10c9694a5112
yu <839177458@qq.com>
2019-12-18 22:22:37 +0800

    提交

---
+++ src/main/java/ai/demo/annotation/tt.java
@@ -0,0 +1,11 @@
+package ai.demo.annotation;
+
+/**
+ * @author yu
+ * @date 2019/12/18 0018
+ */
+public class tt {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
